52a323e68c94bb80c256ee24f21a8c034a35bf66
kishan bhati <bhatikaran634@gmail.com>
2023-06-08 17:01:15 +0530

    fb

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+
+import './App.scss';
+import Navbar from './components/Navbar/Navbar';
+import About from './container/About/About';
+import Footer from './container/Footer/Footer';
+import Header from './container/Header/Header';
+import Skills from './container/Skills/Skills';
+import Testimonial from './container/Testimonial/Testimonial';
+import Work from './container/Work/Work';
+
+
+function App() {
+  return (
+    <div className='app' > 
+    <Navbar/>
+      <Header/>
+      <About/>
+      <Skills/>
+      <Work/>
+      <Testimonial/>
+      <Footer/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/container/Skills/Skills.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Skills.scss'
+
+const Skills = () => {
+  return (
+    <div>
+      Skill
+    </div>
+  )
+}
+
+export default Skills
